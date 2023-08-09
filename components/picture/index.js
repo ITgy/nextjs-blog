df5a466082b509edde6b15510ca7a4dbbbdcc38f
ITgy <muugyj@outlook.com>
2023-08-09 22:48:44 +0800

    代码更新

---
+++ components/picture/index.js
@@ -0,0 +1,50 @@
+import styles from './picture.module.css';
+import {useState, useEffect} from 'react';
+
+export default function Picture({clientX, clientY, bgWidth = 175, bgHeight = 250, width=175, height=300,className = ''}) {
+    const baseBgStyle = {
+        width: `${bgWidth}px`,
+        height: `${bgHeight}px`
+    }
+    const baseStyle = {
+        width: `${width}px`,
+        height: `${height}px`
+    }
+    
+    const [rotateObj, setRotateObj] = useState({
+        ...baseBgStyle
+    });
+    const [imgStyle, setImgStyle] = useState({
+        ...baseStyle
+    });
+
+    
+
+    useEffect(() => {
+        setRotateObj({
+            ...baseBgStyle,
+            // transform: `rotateX(${computedTransform(clientX, window.innerWidth)}deg) rotateY(${computedTransform(clientY, window.innerHeight)}deg)`,
+            backgroundPosition: `${computedBGPosition(clientX, window.innerWidth)} ${computedBGPosition(clientY, window.innerHeight)}`
+        })
+        setImgStyle({
+            ...baseStyle,
+            transform: `translateX(${-computedTransform(clientX, window.innerWidth)}px) translateY(${-computedTransform(clientY, window.innerHeight)}px)`
+        })
+    }, [clientX, clientY])
+
+    // 计算盒子移动幅度
+    function computedTransform(num, doc) {
+        return (num / doc * 40 - 20).toFixed(1);
+    }
+
+    // 计算背景图移动幅度
+    function computedBGPosition(num, doc){
+        return (60 - Number((num / doc * 20).toFixed(1)) + '%');
+    }
+
+    return (
+        <div style={rotateObj} className={`${styles.card} ${className}`}>
+            <img className={styles.img} style={imgStyle} src="/images/avatar2.png"></img>
+        </div>
+    )
+}
--- pages/aboutMe.js
+++ pages/aboutMe.js
@@ -1,5 +1,42 @@
+import styles from '../styles/aboutMe.module.css';
+import { useRef, useState } from 'react';
+import Picture from '../components/picture/index';
 
 
-export default function AboutMe(){
-    return 123
+export default function AboutMe() {
+    const cardObj = useRef(null);
+    const [rotateObj, setRotateObj] = useState(null);
+    const [imgStyle, setImgStyle] = useState(null);
+
+    const [clientX, setClientX] = useState(0);
+    const [clientY, setClienty] = useState(0);
+
+    // 计算盒子移动幅度
+    function computedTransform(num, doc) {
+        return (num / doc * 40 - 20).toFixed(1);
+    }
+
+    // 计算背景图移动幅度
+    function computedBGPosition(num, doc){
+        return (60 - Number((num / doc * 20).toFixed(1)) + '%');
+    }
+
+    function handleMouseMove(e) {
+        setClientX(e.clientX);
+        setClienty(e.clientY);
+
+        setRotateObj({
+            // transform: `rotateX(${computedTransform(e.clientX, window.innerWidth)}deg) rotateY(${computedTransform(e.clientY, window.innerHeight)}deg)`,
+            backgroundPosition: `${computedBGPosition(e.clientX, window.innerWidth)} ${computedBGPosition(e.clientY, window.innerHeight)}`
+        })
+        setImgStyle({
+            transform: `translateX(${-computedTransform(e.clientX, window.innerWidth)}px) translateY(${-computedTransform(e.clientY, window.innerHeight)}px)`
+        })
+    }
+
+    return (
+        <div className={styles.container} onMouseMove={handleMouseMove}>
+            <Picture clientX={clientX} clientY={clientY}></Picture>
+        </div>
+    )
 }
