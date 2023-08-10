4e3eb1d1f94fdac8a18b06873d7438105be92640
gaoxianggui <gaoxianggui@7kid.com>
2023-08-10 20:31:45 +0800

    代码更新

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
-let assetPrefix = '';
-let basePath = '/';
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-if(isGithubActions){
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-    assetPrefix = `/${repo}/`;
-    basePath = `/${repo}`
-}
-
 module.exports = {
-    assetPrefix,
-    basePath,
+    basePath: '/blog-deploy',
+    assetPrefix: '/blog-deploy/',
     experimental: {
         newNextLinkBehavior: false
     }
