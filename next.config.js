deec1f12dc90d67fdf4afeacf45b7a8d58dfd500
gaoxianggui <gaoxianggui@7kid.com>
2023-08-10 14:25:00 +0800

    测试github actions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
+let assetPrefix = '';
+let basePath = '/';
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+if(isGithubActions){
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+    assetPrefix = `/${repo}/`;
+    basePath = `/${repo}`
+}
+
 module.exports = {
+    assetPrefix,
+    basePath,
     experimental: {
         newNextLinkBehavior: false
     }
